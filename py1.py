3b5fc413a8e97123a39909640c0f2c23b73aa475
Aashish Kumar Jaiswal <121435379+AASHISHAI@users.noreply.github.com>
2023-09-11 19:52:08 +0530

    Add files via upload

---
+++ py1.py
@@ -0,0 +1,7 @@
+'''Accept a non zero integer as input Print positive if it is greater than zero and negative if it is less 
+than zero '''
+num=int(input('Enter a number'))
+if num>0:
+    print('Positive')
+else:
+    print('Negative')
