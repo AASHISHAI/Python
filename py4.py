0538dfea38ec2350f7c1bee5e8f836fedbeecfe0
Aashish Kumar Jaiswal <121435379+AASHISHAI@users.noreply.github.com>
2023-09-16 22:56:04 +0530

    Add files via upload

---
+++ py4.py
@@ -0,0 +1,7 @@
+'''Take two int value from user and print greatest amoung them'''
+a=int(input('enter a value :'))
+b=int(input('enter b value :'))
+if a>b:
+    print('Greatest number is :',a)
+else:
+    print('Greatest number is :',b)
